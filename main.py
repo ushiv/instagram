e00247396738eea52b03b51b91e742d025202663
ushiv <89530497+ushiv@users.noreply.github.com>
2021-08-25 19:56:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1995504342:AAFIx_SZRUWTMAEVRp_njOpzWErwkSecS_o"
 
 def download(bot, update):
     message = update.effective_message
